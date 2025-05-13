fe40897606f447c4ef9c37ac7e9a8b583ef85e83
sazconf <md.sazzad.ft24@mail.umy.ac.id>
2025-05-13 10:51:22 +0700

    start else condition

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -1,4 +1,3 @@
-// 4NIMBelakang_DoublelinkedList
 #include <iostream>
 using namespace std;
 
@@ -7,39 +6,44 @@ struct Node {
     // deklarasi noMHS dan name untuk menampung data
     int noMHS;
     string name;
-    // deklarasi pointer next dan pref untuk data sebelum dan sesudah
-    Node*next;
-    Node*prev;
+    // deklarasi pointer next dan prev untuk data sebelum dan sesudah
+    Node* next;
+    Node* prev;
 };
 
 // deklarasi pointer START dan pemberian nilai
-Node*START = NULL;
+Node* START = NULL;
 
-// deklarasi prosedur addnote
+// deklarasi prosedur addnode
 void addnode() {
-
-    // pembuatan node dan pemberian value untuk data noMHS dan name 
-    Node *newNode = new Node(); // step 1 buat node baru
+    // pembuatan node dan pemberian value untuk data noMHS dan name
+    Node* newNode = new Node(); // step 1 buat node baru
     cout << "\nEnter the roll number of the student : ";
     cin >> newNode->noMHS; // assign value to the data field of the new node
-    cout << "\nEnter the same of the student : ";
-    cin >> newNode->name; // Assign value to the data field of th
+    cout << "\nEnter the name of the student : ";
+    cin >> newNode->name; // Assign value to the data field of the new node
 
     // Insert the new node in the list
-    // kondisi jika star == null atau noMHS node baru <= noMHS start
+    // kondisi jika START == NULL atau noMHS node baru <= noMHS START
     if (START == NULL || newNode->noMHS <= START->noMHS) {
-        // step 2: insert the new code at the beginning
-        // kondisi jika star tidak kosong dan noMHS node baru sama dengan noMHS 
+        // step 2: insert the new node at the beginning
+        // kondisi jika START tidak kosong dan noMHS node baru sama dengan noMHS START
         if (START != NULL && newNode->noMHS == START->noMHS) {
-            cout << "\033[31mDuplicate roll numbers not allowed\33[0m" << endl;
+            cout << "\033[31mDuplicate roll numbers not allowed\033[0m" << endl;
+            delete newNode; // to free the allocated memory for the new node
             return;
         }
         // if the list is empty, make the new node the start
-        // jika list kosong, maka node nextnya adalah start
-        newNode->next = START; // step 3: make the new node point to the  first node
-        //kondisi jika start tidak memiliki nilai atau tidak kosong
+        newNode->next = START; // step 3: make the new node point to the first node
         if (START != NULL) {
-            START->prev = newNode; //step 4: make the first node point to the
+            START->prev = newNode; // step 4: make the first node point to the new node
         }
+        newNode->prev = NULL; // step 5 : make the new node's prev pointer null
+        START = newNode; // step 6 : make the new node the first node
+    }
+    else {
+        
     }
 }
+
+
