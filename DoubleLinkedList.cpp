0f7749b40e3bb15470d9c53210f7beb046dbcfb7
sazconf <md.sazzad.ft24@mail.umy.ac.id>
2025-05-13 10:20:11 +0700

    Create struct for node

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -1,3 +1,14 @@
-#include<iostream>
-
+// 4NIMBelakang_DoublelinkedList
+#include <iostream>
 using namespace std;
+
+// membuat struktur untuk object double linkedList
+struct Node {
+    // deklarasi noMHS dan name untuk menampung data
+    int noMHS;
+    string name;
+    // deklarasi pointer next dan pref untuk data sebelum dan sesudah
+    Node*next;
+    Node*prev;
+};
+
