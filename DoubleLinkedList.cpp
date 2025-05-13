79326b0a662a91ef1065926e3c36a350c47dc6b6
sazconf <md.sazzad.ft24@mail.umy.ac.id>
2025-05-13 10:24:27 +0700

    First case

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -28,6 +28,12 @@ void addnode() {
     // Insert the new node in the list
     // kondisi jika star == null atau noMHS node baru <= noMHS start
     if (START == NULL || newNode->noMHS <= START->noMHS) {
+        // step 2: insert the new code at the beginning
+        // kondisi jika star tidak kosong dan noMHS node baru sama dengan noMHS 
+        if (START != NULL && newNode->noMHS == START->noMHS) {
+            cout << "\033[31mDuplicate roll numbers not allowed\33[0m" << endl;
+            return;
+        }
         
     }
 }
